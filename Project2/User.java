9316a206ed57614dc121474fd41b6472488c32f0
Stephen Ranshous <sranshous@gmail.com>
2013-04-01 23:33:01 -0400

    User is a storage class for client info It will be used to keep their name, IPs, and port they connected on.

---
+++ Project2/User.java
@@ -0,0 +1,41 @@
+/**
+ * Describes the important features of a user that will be required to
+ * have clients connect to other clients.
+ * @author Stephen Ranshous
+ */
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class User {
+    private InetAddress userAddress;
+    private int userPort;
+    private String name;
+
+    /**
+     * Create a User with the given address and port that they connected on.
+     * @param name The name of the user
+     * @param userAddress The InetAddress of the user. This will mostly be used
+     * for getting their IP.
+     * @param userPort The port the user connected on.
+     */
+    public User(String name, InetAddress userAddress, int userPort) {
+        this.userAddress = userAddress;
+        this.userPort = userPort;
+    }
+
+    /**
+     * Get the InetAddress of this user.
+     * @return The InetAddress for the user.
+     */
+    public InetAddress getAddress() {
+        return userAddress;
+    }
+
+    /**
+     * Get the port this user connected on.
+     * @return The port they connected on.
+     */
+    public int getPort() {
+        return userPort;
+    }
+}
